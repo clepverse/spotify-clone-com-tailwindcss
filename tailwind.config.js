77c9fbd45ea2e0ffbd1c2cec7e3359ca474d8c48
Aldo Migge <cleepe5@hotmail.com>
2022-01-14 15:05:21 -0300

    Projeto configurado/tailwind.config  extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        14: "3.5rem",
+        "125px": "125px",
+      },
+      colors: {
+        hoverspt: "#18D760",
+        "purple-main": "#2D46B9",
+        "green-main": "#1ED760",
+      },
+      backgroundImage: (theme) => ({
+        "spotify-theme": "url('/src/img/bursts.svg')",
+        "spotify-theme-mobile": "url('/src/img/bursts-mobile.svg')",
+      }),
+      backgroundSize: {
+        "175%": "185%",
+        "195%": "195%",
+      },
+      backgroundPosition: {
+        banner: "45% 15%",
+        "banner-mobile": "top 25% center",
+      },
+      fontSize: {
+        "9xl": "9rem",
+      },
+    },
   },
   plugins: [],
 };
